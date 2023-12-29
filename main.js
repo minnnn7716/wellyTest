68864743b85af8be83ebc77a77a7a59bd197af0e
m_m <minnn7716@gmail.com>
2023-12-29 14:08:58 +0800

    feat: 補上 React 題目、Readme

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-// 我是於 Yourator 應徵 前端工程師 的王咨閔
-
 // 【題目一】JavaScript: 字串反轉
 function reverseString(str) {
   return str.split('').reverse().join('');
@@ -20,7 +18,7 @@ console.log(filterNumbersGreaterThanFive(numbers)); // 預期輸出: [8, 10, 7]
 
 
 
-// 【題目二】JavaScript: 重構
+// 【題目三】JavaScript: 重構
 // 重構這段程式碼並說明原因
 // 以下為已重構程式碼
 // 原始程式碼主要想判斷使用者輸入的姓名取真值組合字串。我重構為以下寫法的原因是，直接將參數展開並篩選真值，簡化撰寫行數並增加參數彈性，例如往後想增加中間名參數變不須修改。篩選陣列後再組合的方式也可避免原先寫法僅有 lastName 沒有 firstName 時，會產生多餘的空白字元。
@@ -30,3 +28,51 @@ function formatName(firstName, lastName) {
 
   return formatAry.join(' ');
 }
+
+
+// 【題目四】React: 條件渲染
+const { useState } = React;
+
+function ConditionalRendering({ isLoggedIn }) {
+  const [logStatus, setLogStatus] = useState(isLoggedIn);
+  
+  return (
+    <div>
+      <h3>【題目四】React: 條件渲染</h3>
+      <div className="d-flex align-items-center">
+        <p className="me-2">{ logStatus ? '已登入' : '未登入' }</p>
+        <button type="button" onClick={ () => setLogStatus(!logStatus) }>
+          { logStatus ? '登出' : '登入' }
+        </button>
+      </div>
+    </div>
+  )
+}
+
+
+// 【題目五】React: 組件
+function Counter() {
+  const [num, setNum] = useState(0);
+  
+  return (
+    <div>
+      <h3>【題目五】React: 組件</h3>
+      <div className="d-flex align-items-center">
+        <button type="button" onClick={ () => setNum(num - 1) }>－</button>
+        <p className="mx-2">{num}</p>
+        <button type="button" onClick={ () => setNum(num + 1) }>＋</button>
+      </div>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <>
+      <ConditionalRendering isLoggedIn={ false } />
+      <Counter />
+    </>
+  )
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
